de420ff90b4104ea6c1e354678e961bcb15a31c2
Jabbar Ali <jabbar.ali@mis-net.co.uk>
2023-06-11 21:30:44 +0100

    change time

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello! 20:52
+          Hello! 21:26
         </p>
         
       </header>
